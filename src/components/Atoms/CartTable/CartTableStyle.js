492879c8d059f6523ceb8b519bd1828fc6ceb341
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2021-04-15 18:06:11 +0200

    Add style to CartTableStyle

--- src/components/Atoms/CartTable/CartTableStyle.js
+++ src/components/Atoms/CartTable/CartTableStyle.js
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const CartTableWrapper = styled.div.attrs((props) => ({
   className: "row",
 }))`
-  border-bottom: 1px solid ${({ theme }) => theme.grey};
+  border-bottom: 0.1rem solid ${({ theme }) => theme.grey};
+  margin-bottom: 2rem;
 `;
