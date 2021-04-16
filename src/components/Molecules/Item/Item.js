61d06b82a08a056384a024900f5abc6e58814fae
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2021-04-16 12:29:24 +0200

    Styling item price section in Item component

--- src/components/Molecules/Item/Item.js
+++ src/components/Molecules/Item/Item.js
@@ -1,9 +1,18 @@
-import { ItemSection, ItemTitle, ItemInfo, ItemImage } from "./ItemStyle";
 import Paragraph from "../../Atoms/Paragraph/Paragraph";
+import { formatPrice } from "../../../helpers";
+import {
+  ItemRow,
+  ItemTitle,
+  ItemInfo,
+  ItemImage,
+  ItemPrice,
+  ItemActions,
+  ItemQuantity,
+} from "./ItemStyle";
 
 const Item = ({ items }) => {
   return items.map((item) => (
-    <ItemSection key={item.id}>
+    <ItemRow key={item.id} extra="itemSection">
       <ItemInfo>
         <ItemImage src={item.image} alt={item.name} />
         <ItemTitle>
@@ -11,16 +20,16 @@ const Item = ({ items }) => {
           <Paragraph extra="light" title={`Kategoria: ${item.category}`} />
         </ItemTitle>
       </ItemInfo>
-      <div className="col-lg-7">
-        <div className="row">
-          <div className="col-lg-5">
+      <ItemActions>
+        <ItemRow>
+          <ItemQuantity>
             Max: {item.max}
             Min: {item.min}
-          </div>
-          <div className="col-lg-7">{item.price}</div>
-        </div>
-      </div>
-    </ItemSection>
+          </ItemQuantity>
+          <ItemPrice>{formatPrice(item.price)}</ItemPrice>
+        </ItemRow>
+      </ItemActions>
+    </ItemRow>
   ));
 };
 
--- src/components/Molecules/Item/ItemStyle.js
+++ src/components/Molecules/Item/ItemStyle.js
@@ -1,10 +1,17 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const ItemSection = styled.div.attrs((props) => ({
+export const ItemRow = styled.div.attrs((props) => ({
   className: "row",
 }))`
-  margin-bottom: 20px;
-  align-items: center;
+  ${(props) =>
+    props &&
+    props.extra === "itemSection" &&
+    css`
+      margin-bottom: 25px;
+      padding-bottom: 20px;
+      align-items: center;
+      border-bottom: 1px solid ${({ theme }) => theme.grey};
+    `}
 `;
 
 export const ItemInfo = styled.div.attrs((props) => ({
@@ -25,4 +32,22 @@ export const ItemTitle = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   justify-content: flex-start;
+`;
+
+export const ItemQuantity = styled.div.attrs((props) => ({
+  className: "col-lg-6",
+}))`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const ItemActions = styled.div.attrs((props) => ({
+  className: "col-lg-7",
+}))``;
+
+export const ItemPrice = styled.div.attrs((props) => ({
+  className: "col-lg-6",
+}))`
+  display: flex;
+  justify-content: flex-end;
 `;
