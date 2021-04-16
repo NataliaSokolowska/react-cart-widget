ed4fee477cf02aa95e56230a75939c19d9da19a9
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2021-04-16 12:28:01 +0200

    Changing styled for CartTable - added flex-end

--- src/components/Atoms/CartTable/CartTableStyle.js
+++ src/components/Atoms/CartTable/CartTableStyle.js
@@ -1,8 +1,24 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const CartTableWrapper = styled.div.attrs((props) => ({
   className: "row",
 }))`
   border-bottom: 0.1rem solid ${({ theme }) => theme.grey};
   margin-bottom: 2rem;
+`;
+
+export const CartTableFifth = styled.div.attrs((props) => ({
+  className: "col-lg-5",
+}))``;
+
+export const CartTableSeventh = styled.div.attrs((props) => ({
+  className: "col-lg-7",
+}))`
+  ${(props) =>
+    props &&
+    props.extra === "textRight" &&
+    css`
+      display: flex;
+      justify-content: flex-end;
+    `}
 `;
